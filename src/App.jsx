12d4bea27fe75755d7a2d648758f3b4b77b2420c
Francisco Galarza <galarzafrancisco98@gmail.com>
2022-06-13 12:05:49 -0300

    Agregando carpeta Components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="container"> 
+    <div className="App" >
+    </div>
+    <NavBar/>
+    </div>
+  );
+}
+
+export default App;
