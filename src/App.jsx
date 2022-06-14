c7ab9d13cd7485d6f489c9009d886882f5af51c1
Francisco Galarza <galarzafrancisco98@gmail.com>
2022-06-14 17:32:22 -0300

    respaldo 2 entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
-import NavBar from './components/NavBar';
+
+
 
 function App() {
   return (
     <div className="container"> 
     <div className="App" >
     </div>
-    <NavBar/>
+  
     </div>
   );
 }
