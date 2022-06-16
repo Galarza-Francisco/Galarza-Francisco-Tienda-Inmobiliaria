60330a43798f996b1aa3255866ba5b5b04a2ef94
Francisco Galarza <galarzafrancisco98@gmail.com>
2022-06-16 19:15:27 -0300

    agregando chakra UI al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import './App.css';
-import ItemListContainer from './components/Containers/ItemListContainer';
-import Input from './components/Input';
-import NavBar from "./components/NavBar";
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <Input valorPlaceholder={"hola"} title={" como estas?"}/>
-      <ItemListContainer greeting={"hola coders!"}/>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import './NavBar.css';
-const NavBar = () =>{
-    return (
-        <ul >
-            <li><a href="#home">Inicio</a></li>
-            <li><a href="#news">Tasaciones</a></li>
-            <li><a href="#contact">Quienes Somos</a></li>
-            <li><a href="#about">Contacto</a></li>
-        </ul>
-    )
+
+import { Flex, Heading, HStack, Link } from '@chakra-ui/react'
+import { Image } from '@chakra-ui/react'
+import React from 'react'
+
+function NavBar() {
+  return (
+    <Flex px='200px' py={'30px'} width='full' justifyContent='space-between' alignItems={'flex-end'}>
+        <Flex alignItems={'flex-end'}>
+            <Heading mr={'60px'} fontSize={20} letterSpacing='1.5px' color={'yellow.400'}>
+               <Image
+                borderRadius={'full'}
+                boxSize='100px'
+                src='https://bit.ly/dan-abramov'
+                alt='Dan Abramov'
+                />
+            </Heading>
+            <HStack spacing={'40px'}>
+                <Link>Inicio</Link>
+                <Link>Tasaciones</Link>
+                <Link>Nosotros</Link>
+                <Link>Contacto</Link>
+            </HStack>
+        </Flex>
+        <Link>
+            Ingresar
+        </Link>
+    </Flex>
+  )
 }
 
 export default NavBar
